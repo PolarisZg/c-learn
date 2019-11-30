88a251db5daa416c55a933427a0ad6bb9ee08e5c
quancheng129 <55468046+quancheng129@users.noreply.github.com>
2019-11-30 21:47:53 +0800

    Create 题目4.cpp

---
+++ 题目4.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(void)
+{
+	int a;
+	scanf("%d",&a);
+	if(a>=90&&a<=100)
+	printf("A");
+	else if(a<90&&a>=80)
+	printf("B");
+	else if(a<80&&a>=70)
+	printf("C");
+	else if(a<70&&a>=60)
+	printf("D");
+	else if(a<60&&a>=0)
+	printf("E");
+	else
+	printf(" ‰»Î≥…º®”–ŒÛ");
+	return 0;	
+ } 
