e9419bb1c72f81ef1692df4cd7f4afe5033689c3
quancheng129 <55468046+quancheng129@users.noreply.github.com>
2019-11-30 21:45:52 +0800

    Create 2.cpp

---
+++ 2.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int x,y,a,b,c;
+	y=0;
+	a=x/100;
+	b=(x/10)%10;
+	c=x%10;
+	for(x=101;x<200;x++)
+	if(x%2==0&&a!=b&&b!=c&&a!=c)
+	{
+		y=y+1;
+		printf("%6d",x);
+  		if(y%10==0) printf("\n");
+	}
+	return 0;
+}
+
